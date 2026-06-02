d84404a56174931aa964eae56451d41050902104
rg518 <regisndemezoo@gmail.com>
2026-06-02 21:08:16 +0100

    Fix: Remove unused useState import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import HomePage from './pages/HomePage'
